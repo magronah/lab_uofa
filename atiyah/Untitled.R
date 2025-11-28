0c7c1442c70f7a0f07d392677d33e3cb0c09433c
Atiyah Sulaiman <atiyah@Atiyahs-MacBook-Pro.local>
2025-11-28 15:07:49 -0700

    commit initial

--- atiyah/Untitled.R
+++ atiyah/Untitled.R
@@ -2,6 +2,6 @@
 library(power.nb)
 library(readr)
 #read dataset
-countdata = read_tsv("ob_ross/ob_ross_ASVs_table.tsv")
+countdata = read_tsv(~"ob_ross/ob_ross_ASVs_table.tsv")
 View(countdata)
 dim(countdata)
